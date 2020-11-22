e86aeaff89db87a1dc4c1b600a58216f0dd0c496
Stelios Papadopoulos <stelios@spapa.us>
2020-11-22 02:39:27 -0600

    add ipywidget imports to __init__.py

--- wridgets/__init__.py
+++ wridgets/__init__.py
@@ -1,3 +1,6 @@
+from ipywidgets import widgets, HBox, VBox, Label, Layout, GridBox, Tabs, Stacked
+from IPython.display import display, clear_output, FileLink
+
 from .wrappers import IntSlider, FloatSlider, FloatLogSlider, IntRangeSlider, FloatRangeSlider, IntProgress, FloatProgress, BoundedIntText, \
     BoundedFloatText, IntText, FloatText, ToggleButton, Checkbox, Valid, Dropdown, RadioButtons, Select, SelectionSlider, SelectionRangeSlider, \
         ToggleButtons, SelectMultiple, Text, Textarea, Combobox, Password, Play, DatePicker, ColorPicker, FileUpload, Image, Button
