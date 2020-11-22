34c6bb274606ffaf84b4e1531709484e3618d5e8
Stelios Papadopoulos <stelios@spapa.us>
2020-11-22 02:38:09 -0600

    add imports to __init__.py

--- wridgets/__init__.py
+++ wridgets/__init__.py
@@ -1,1 +1,3 @@
-from .wrappers import Button, Checkbox, Dropdown
+from .wrappers import IntSlider, FloatSlider, FloatLogSlider, IntRangeSlider, FloatRangeSlider, IntProgress, FloatProgress, BoundedIntText, \
+    BoundedFloatText, IntText, FloatText, ToggleButton, Checkbox, Valid, Dropdown, RadioButtons, Select, SelectionSlider, SelectionRangeSlider, \
+        ToggleButtons, SelectMultiple, Text, Textarea, Combobox, Password, Play, DatePicker, ColorPicker, FileUpload, Image, Button
