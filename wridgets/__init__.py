1482bde29f8b6ae8a85e35d8374032df4cc414c7
spapa013 <stelios@spapa.us>
2022-08-30 22:26:32 -0500

    add version check to init

--- wridgets/__init__.py
+++ wridgets/__init__.py
@@ -1,3 +1,7 @@
 from .utils import GridBox2
 from .version import __version__
 from .wridgets import *
+import logging
+from ipywidgets import __version__ as ipywidgets_version
+if int(ipywidgets_version.split('.')[0]) < 8:
+    logging.warning(f'you have ipywidgets version {ipywidgets_version} which is incompatible with some features of wridgets version {__version__}.')
