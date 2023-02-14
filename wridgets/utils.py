58f5ff631982c1a53814ab10250258eeb8eafec2
spapa013 <stelios@spapa.us>
2023-02-14 11:58:16 -0600

    update GridBox2

--- wridgets/utils.py
+++ wridgets/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-from ipywidgets import VBox, HBox 
+from ipywidgets import HBox, VBox
 
 
 def wrap(item):
@@ -15,30 +15,26 @@ def unwrap(item):
     return item
 
 
-def GridBox2(items:list, dims:tuple=(3, -1)):
+def GridBox2(items: list, dims: tuple = (3, -1)):
     """
     Displays multiple widgets in rows and columns.
-    
+
     :param items: (list) widgets to display
     :param dims: (tuple) n_rows, n_cols
-    
+
     :returns: VBox with widgets
     """
     n_items = len(items)
     n_rows, n_cols = dims
-    assert (n_rows > 0) or (n_cols > 0), 'At least one dim must be > 0'
-    assert (n_rows != 0) and (n_cols != 0), 'dims cant contain 0'
+    assert (n_rows > 0) or (n_cols > 0), "At least one dim must be > 0"
+    assert (n_rows != 0) and (n_cols != 0), "dims cant contain 0"
     n_rows = int(np.ceil(n_items / n_cols)) if n_rows < 0 else n_rows
     n_cols = int(np.ceil(n_items / n_rows)) if n_cols < 0 else n_cols
-    assert n_rows * n_cols >= n_items, f"Specified dims: {n_rows, n_cols} won't fit all {n_items} items"
+    assert (
+        n_rows * n_cols >= n_items
+    ), f"Specified dims: {n_rows, n_cols} won't fit all {n_items} items"
 
     HBoxs = []
     for r in range(n_rows):
-        HBoxs.append(
-            HBox(
-                *[items[slice(n_cols*r, n_cols*(r+1))]]
-            )
-        )
+        HBoxs.append(HBox(*[items[slice(n_cols * r, n_cols * (r + 1))]]))
     return VBox([*HBoxs])
-
-
